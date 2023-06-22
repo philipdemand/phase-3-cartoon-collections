d25caeb70e15011ed04b40c9847fd5098c04e745
Philip Demand <philipdemand@yahoo.com>
2023-06-22 15:27:14 -0400

    commit tree 698b70d606e4f1ecc8cc30fd874055fc894ac0ee

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = array.map do |name|
+    name.capitalize + "!"
+  end
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  if array.include?("gouda")
+    "gouda"
+  elsif array.include?("cheddar")
+    "cheddar"
+  elsif array.include?("camembert")
+    "camembert"
+  else
+    nil
+  end
 end
+
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
